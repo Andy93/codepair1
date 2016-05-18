f435e268c4139f71d7e82a8f96afb6a90ced9c6b
Andy93 <oneillandrew101@gmail.com>
2016-05-18 22:08:20 +0100

    Add files via upload

---
+++ config/databases.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://Andrew_ONeill:brandy101@ds023540.mlab.com:23540/codepair' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
